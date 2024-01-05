56278b9c5cb9edca05f1b4468477e9f6600ea4ab
Greg Merritt <gmerritt@salsify.com>
2024-01-05 13:15:28 -0500

    Add Ruby 3.3.0 to Test Matrix (#11)

--- lib/avro_schema_registry/test/fake_server.rb
+++ lib/avro_schema_registry/test/fake_server.rb
@@ -7,16 +7,16 @@ module AvroSchemaRegistry
   class FakeServer < FakeConfluentSchemaRegistryServer
     get '/subjects/:subject/fingerprints/:fingerprint' do
       subject = params[:subject]
-      halt(404, SCHEMA_NOT_FOUND) unless SUBJECTS.key?(subject)
+      halt(404, SCHEMA_NOT_FOUND) unless SUBJECTS[DEFAULT_CONTEXT]&.key?(subject)
 
       fingerprint = params[:fingerprint]
       fingerprint = fingerprint.to_i.to_s(16) if /^\d+$/.match?(fingerprint)
 
-      schema_id = SCHEMAS.find_index do |schema|
+      schema_id = SCHEMAS[DEFAULT_CONTEXT].find_index do |schema|
         Avro::Schema.parse(schema).sha256_resolution_fingerprint.to_s(16) == fingerprint
       end
 
-      halt(404, SCHEMA_NOT_FOUND) unless schema_id && SUBJECTS[subject].include?(schema_id)
+      halt(404, SCHEMA_NOT_FOUND) unless schema_id && SUBJECTS.dig(DEFAULT_CONTEXT, subject)&.include?(schema_id)
 
       { id: schema_id }.to_json
     end
